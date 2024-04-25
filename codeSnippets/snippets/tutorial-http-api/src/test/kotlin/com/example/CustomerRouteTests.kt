7d3fd03b341392938e74bdca14461eb0724e2c11
Duncan McGregor <duncan@oneeyedmen.com>
2024-04-25 14:59:52 +0100

    Add tests for POST

--- codeSnippets/snippets/tutorial-http-api/src/test/kotlin/com/example/CustomerRouteTests.kt
+++ codeSnippets/snippets/tutorial-http-api/src/test/kotlin/com/example/CustomerRouteTests.kt
@@ -59,6 +59,15 @@ class CustomerRouteTests {
         }
 
     @Test
+    fun `returns 400 when no id`() =
+        testApplicationWith(customers, orders) {
+            customers.add(aCustomer)
+            val response = client.get("/customer/")
+            assertEquals(HttpStatusCode.BadRequest, response.status)
+            assertEquals("Missing id", response.bodyAsText())
+        }
+
+    @Test
     fun `deletes a customer by id`() =
         testApplicationWith(customers, orders) {
             customers.add(aCustomer)
@@ -76,5 +85,37 @@ class CustomerRouteTests {
             assertEquals(HttpStatusCode.NotFound, response.status)
             assertEquals("Not Found", response.bodyAsText())
             assertFalse(customers.isEmpty())
+        }
+
+    @Test
+    fun `returns 400 when no id given to delete`() =
+        testApplicationWith(customers, orders) {
+            customers.add(aCustomer)
+            val response = client.delete("/customer/")
+            assertEquals(HttpStatusCode.BadRequest, response.status)
+            assertFalse(customers.isEmpty())
+        }
+
+    @Test
+    fun `adds a customer`() =
+        testApplicationWith(customers, orders) {
+            assertTrue(customers.isEmpty())
+            val response = client.post("/customer") {
+                contentType(ContentType.Application.Json)
+                setBody(aCustomerJson)
+            }
+            assertEquals(HttpStatusCode.Created, response.status)
+            assertEquals(listOf(aCustomer), customers)
+        }
+
+    @Test
+    fun `returns 415 when no customer to add`() =
+        testApplicationWith(customers, orders) {
+            val response = client.post("/customer") {
+                contentType(ContentType.Application.Json)
+                setBody("")
+            }
+            assertEquals(HttpStatusCode.UnsupportedMediaType, response.status)
+            assertTrue(customers.isEmpty())
         }
 }
