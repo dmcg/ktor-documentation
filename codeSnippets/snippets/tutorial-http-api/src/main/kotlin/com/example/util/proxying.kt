700a4a391dd991af62dce133885585a2e3b242ae
Duncan McGregor <duncan@oneeyedmen.com>
2024-07-05 21:15:28 +0100

    Fun with proxies too

--- codeSnippets/snippets/tutorial-http-api/src/main/kotlin/com/example/util/proxying.kt
+++ codeSnippets/snippets/tutorial-http-api/src/main/kotlin/com/example/util/proxying.kt
@@ -1,6 +1,7 @@
 package com.example.util
 
-import java.lang.reflect.Proxy.*
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy.newProxyInstance
 
 inline fun <reified T> fake() = newProxyInstance(
     T::class.java.classLoader,
@@ -9,13 +10,56 @@ inline fun <reified T> fake() = newProxyInstance(
     TODO("No implementation for method $method")
 } as T
 
-inline fun <reified T> Any.structurallyTypedAs() = newProxyInstance(
-    T::class.java.classLoader,
-    arrayOf(T::class.java)
-) { _, method, args ->
-    val matchingMethod = this.javaClass.getMethod(
+inline fun <reified T> Any.structurallyTypedAs(): T =
+    structurallyTypedAs(T::class.java)
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Any.structurallyTypedAs(proxyType: Class<T>): T {
+    val allTypes = this.javaClass.allTypes()
+    val lookup: Map<Method, Method> =
+        proxyType.methods.associateWith { proxyMethod ->
+            allTypes.methodMatching(proxyMethod, thatCanAccess = this) ?:
+                throw NoSuchMethodError("Matching " + proxyMethod.toString())
+        }
+    return newProxyInstance(
+        proxyType.classLoader,
+        arrayOf(proxyType)
+    ) { _, method, args ->
+        lookup[method]?.invoke(this, *(args ?: emptyArray()))
+            ?: error("No implementation for method $method")
+    } as T
+}
+
+private fun Iterable<Class<*>>.methodMatching(
+    method: Method,
+    thatCanAccess: Any
+): Method? = firstNotNullOfOrNull { clazz ->
+    clazz.methodOrNull(
         method.name,
         *method.parameterTypes
-    )
-    matchingMethod.invoke(this, *(args ?: emptyArray()))
-} as T
+    )?.takeIf { it.canAccess(thatCanAccess) }
+}
+
+private fun Class<*>.methodOrNull(
+    name: String,
+    vararg parameterTypes: Class<*>
+): Method? = try {
+    getMethod(name, *parameterTypes)
+} catch (x: NoSuchMethodException) {
+    null
+}
+
+internal fun Class<*>.allTypes(
+    soFar: MutableList<Class<*>> = mutableListOf()
+): Set<Class<*>> {
+    soFar.add(this)
+    val interfaces = this.interfaces
+    soFar.addAll(interfaces)
+    interfaces.forEach {
+        it.allTypes(soFar)
+    }
+    return if (this.superclass == null)
+        soFar.toSet()
+    else
+        this.superclass.allTypes(soFar)
+}
--- codeSnippets/snippets/tutorial-http-api/src/test/kotlin/com/example/util/StructuralTypingTests.kt
+++ codeSnippets/snippets/tutorial-http-api/src/test/kotlin/com/example/util/StructuralTypingTests.kt
@@ -1,22 +1,45 @@
 package com.example.util
 
 import org.junit.jupiter.api.Test
-import java.util.ArrayList
+import org.junit.jupiter.api.assertThrows
+import java.io.Serializable
 import kotlin.test.assertEquals
 
 class StructuralTypingTests {
 
     @Test
     fun test() {
-        val wrapped = ArrayList(listOf("banana", "kumquat"))
+        val wrapped = listOf("banana", "kumquat")
         val wrapper = wrapped.structurallyTypedAs<MyCollection<String>>()
         assertEquals(2, wrapper.size())
         assertEquals("kumquat", wrapper.get(1))
     }
-}
 
+    @Test
+    fun `throws method not found on creation if can't find a match`() {
+        val wrapped = listOf("banana", "kumquat")
+        assertThrows<NoSuchMethodError> {
+            wrapped.structurallyTypedAs<MyCollection2<String>>()
+        }
+    }
+
+    @Test
+    fun `find all types`() {
+        val thing = "hello"
+        assertEquals(
+            setOf(String::class.java, Serializable::class.java, Comparable::class.java, CharSequence::class.java, java.lang.constant.Constable::class.java, java.lang.constant.ConstantDesc::class.java, Object::class.java),
+            thing::class.java.allTypes()
+        )
+    }
+}
 
 interface MyCollection<E> {
     fun size(): Int
     fun get(index: Int): E
+}
+
+interface MyCollection2<E> {
+    fun size(): Int
+    fun get(index: Int): E
+    fun nosuch(): Any
 }
